a7225b1f054c61e77db04b627bfbf116d7df1c6e
IanWood1 <75152913+IanWood1@users.noreply.github.com>
2021-10-22 18:20:25 -0500

    improved dropdown

--- src/components/sidebar/Dropdown.jsx
+++ src/components/sidebar/Dropdown.jsx
@@ -1,14 +1,59 @@
+import { useState } from "react";
+import {Multiselect} from 'multiselect-react-dropdown';
+
+// https://wdeva22.medium.com/implement-multi-select-box-in-reactjs-6fa222ccd9f9
+
+export default function Dropdown() {
+    const data = [
+        {id:1,group:"Saftey Sensors"},
+        {id:2,group:"Chasis Sensors"},
+        {id:3,group:"Aero Sensors"},
+        {id:4,group:"Suspension Sensors"},
+        {id:5,group:"Powertrain Sensors"}
+     ]
+   const [options] = useState(data);
+   return (
+        <div>
+           <div>Select Sensor Group</div>
+           <Multiselect options={options} displayValue="group" />
+        </div>
+   );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 import { MultiSelect } from "@progress/kendo-react-dropdowns";  
 import { useState } from "react";  
 
 
-/*
+
 npm install --save @progress/kendo-react-dropdowns @progress/kendo-react-treeview 
 @progress/kendo-react-animation @progress/kendo-react-intl @progress/kendo-react-data-tools 
 @progress/kendo-react-common @progress/kendo-data-query @progress/kendo-react-buttons 
 @progress/kendo-react-dateinputs @progress/kendo-react-inputs @progress/kendo-drawing
 @progress/kendo-licensing @progress/kendo-theme-default 
-*/
+
 
 const groups = [
     "Saftey Sensors",
@@ -19,12 +64,16 @@ const groups = [
 ];
 
 export default function Dropdown(){
-    const [selectedTags, setSelectedTags] = useState([]);  
-    const onChange = event => setSelectedTags([...event.value]);  
+    const [selectedGroups, setSelectedGroups] = useState([]);  
+    const onChange = event => setSelectedGroups([...event.value]);  
 
     return(
         <div>
-            <MultiSelect data={groups} value={selectedTags} onChange={onChange}/>
+            <MultiSelect data={groups} value={selectedGroups} onChange={onChange}/>
         </div>  
     );
 };
+
+
+
+*/

83ca0c3382b61cce700c219e32336fd47ef60d67
izo0619 <isabelzhong30@gmail.com>
2021-10-23 13:59:49 -0500

    graphs generated, need to work on dimensioning

--- src/components/main/data-display/Graphs.jsx
+++ src/components/main/data-display/Graphs.jsx
@@ -1,9 +1,15 @@
-//import styled from 'styled-components';
-
+import styled from 'styled-components';
+import Graph from './Graph';
+import VertSpacer from '../../shared/VertSpacer';
 export default function Graphs(props) {
   return(
-    <div>
-      ** {props.viewState} **
-    </div>
+    <>
+      <Graph></Graph>
+      <VertSpacer/>
+      <Graph></Graph>
+      <VertSpacer/>
+      <Graph></Graph>
+    </>
   );
 }
+

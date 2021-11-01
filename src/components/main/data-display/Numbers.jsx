1cd79c76067973728863dc8854d7f2322c51cb26
Sean Rhee <seanrhee2024@u.northwestern.edu>
2021-11-01 03:17:13 -0400

    Added Numbers

--- src/components/main/data-display/Numbers.jsx
+++ src/components/main/data-display/Numbers.jsx
@@ -1,9 +1,32 @@
-//import styled from 'styled-components';
+import styled from 'styled-components';
+
+import Number from "./Number";
 
 export default function Numbers() {
+  let data = new Array(11).fill(null).map((_,i) =>
+    <Number
+      value={Math.random()*30}
+      percentage={Math.random()}
+      unit={'m/s'}
+      label={'Wheel Speed'}
+      key={i}
+    />
+  );
+
   return(
-    <div>
-      **NUMBERS PLACEHOLDER**
-    </div>
+    <NumberTray>
+      {data}
+    </NumberTray>
   );
 }
+
+const NumberTray = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: -12px;
+  > * {
+    margin-top: 12px;
+  }
+`;

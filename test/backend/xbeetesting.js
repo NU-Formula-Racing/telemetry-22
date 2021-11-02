f8247a23b076ee3de8bdbddc838c4862caff931e
a11ce <a11ce@protonmail.com>
2021-11-01 20:15:30 -0500

    print raw xbee data

--- test/backend/xbeetesting.js
+++ test/backend/xbeetesting.js
@@ -1,83 +1,45 @@
 const { time } = require('console');
 let SerialPort = require('serialport'); //.SerialPort
 let xbee_api = require('xbee-api');
-let xbeeRx = require('xbee-rx');
 var C = xbee_api.constants;
 
-//document.write("baba baba");
 
 var xbeeAPI = new xbee_api.XBeeAPI({
     api_mode: 1
   });
 
-// function sleep(ms) {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, ms);
-//   });
-// }
 
-// let getPortsList = () => {
-//   SerialPort.list().then(ports => {
-//     ports.forEach(function(port) {
-//       console.log(port);
-//       if (port.serialNumber == 'D306E0R6') {
-//         let serialport = new SerialPort(port.path, {baudRate: 57600});
-//         console.log(serialport)
-//       }
-//     });
-//   });
-// };
+ let getPortsList = () => {
+   SerialPort.list().then(ports => {
+     ports.forEach(function(port) {
+       //console.log(port);
+       if (port.serialNumber == 'D306E0R6') {
+         //let serialport = new SerialPort(port.path, {baudRate: 57600});
+         console.log(port)
+       }
+     });
+   });
+ };
 
+//var portPath;
+//let getPort = () => {SerialPort.list().then(
+ //   ports => {
+    
+   // portPath = ports.find(port => port.serialNumber == 'D306E0R6').path);
 
-//getPortsList();
+//    });
+//};
 
-var serialport = new SerialPort("COM7", {
+//console.log(getPortsList());
+
+console.log("ur on ur own for port path lmao")
+var serialport = new SerialPort("/dev/tty.usbserial-D306E0R6", {
   baudRate: 9600,
   parser: xbeeAPI.rawParser()
 });
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
-// var xbee = xbeeRx({
-//   serialport: 'COM7',
-//   serialportOptions: {
-//       baudRate: 9600
-//   },
-//   module: "ZigBee",
-//   api_mode: 1
-// });
-
-
-  
-// All frames parsed by the XBee will be emitted here
-
-xbeeAPI.on("frame_object", function(frame) {
-    console.log(">>", frame);
-});
-
-// xbeeAPI.parser.on("data", function(frame) {
-//   console.log(">>", frame);
-// });
-
 serialport.on('data', function (data) {
-  // try {
-  //     xbeeAPI.parseRaw(data);
-  // } catch (e) {
-  //     console.error(e);
-  // }
-  xbeeAPI.on("frame_object", function (frame) {
-      console.log(frame);
-      // do what do you want with the frame
-  });
+    console.log(data);
 });
-// subscription = xbee.allPackets
-//     .subscribe(function (packet) {
-//         console.log(packet);
-//     });
-
-console.log("hi");
-
-// while (true) {
-//   sleep(1);
-// }
-

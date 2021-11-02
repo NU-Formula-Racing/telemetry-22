529f181d3d67a1cb07bc42707ffb6fd6b84b3a0b
offi-DtrGuo-cial <offi.dtrguo.cial@gmail.com>
2021-11-01 19:21:46 -0500

    Test script for file editing, namely writing
    Script includes basic writing and appending functions.

---
+++ test/backend/fs-test.js
@@ -0,0 +1,13 @@
+let fs = require('fs')
+
+fs.writeFile('test-file.txt', 'Hello there.\n', function (err) {
+    if (err) throw err;
+    console.log('File added.');
+});
+
+for (let i = 0; i < 4; i++) {
+    fs.appendFile('test-file.txt', 'General Kenobi.\n', function (err) {
+        if (err) throw err;
+        console.log('New line written.');
+    });
+}

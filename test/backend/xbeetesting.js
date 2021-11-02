07d22ddd7b474b299dab260d65aeca6d6284468a
a11ce <a11ce@protonmail.com>
2021-11-02 15:28:29 -0500

    Formatting

--- test/backend/xbeetesting.js
+++ test/backend/xbeetesting.js
@@ -1,4 +1,6 @@
-const { time } = require('console');
+const {
+    time
+} = require('console');
 let SerialPort = require('serialport'); //.SerialPort
 let xbee_api = require('xbee-api');
 var C = xbee_api.constants;
@@ -6,38 +8,32 @@ var C = xbee_api.constants;
 
 var xbeeAPI = new xbee_api.XBeeAPI({
     api_mode: 1
-  });
+});
 
+let findAndListenToXBee = () => {
+    SerialPort.list().then(ports => {
+        ports.forEach(function(port) {
+            if (port.serialNumber == 'D306E0R6') {
+                listenToPortPath(port.path)
+            }
+        });
+    });
+};
 
- let findAndListenToXBee = () => {
-   SerialPort.list().then(ports => {
-     ports.forEach(function(port) {
-       if (port.serialNumber == 'D306E0R6') {
-         listenToPortPath(port.path)
-       }  
-     })
-     ;
-   });
- };
+let listenToPortPath = (portPath) => {
 
-let listenToPortPath = (portPath) =>
-{
-   
     var serialport = new SerialPort(portPath, {
         baudRate: 9600,
         parser: xbeeAPI.rawParser()
     });
 
-    
     serialport.pipe(xbeeAPI.parser);
     xbeeAPI.builder.pipe(serialport);
 
-    
-    serialport.on('data', function (data) {
+    serialport.on('data', function(data) {
         console.log(data);
     });
 }
 
 console.log("im gonna try to find and start listening to the xbee!\nif the script exits right away, the xbee wasnt found :(")
 findAndListenToXBee();
-

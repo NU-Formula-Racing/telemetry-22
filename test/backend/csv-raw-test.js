799780e7f3de2c60885e5a2562b72601aa1b9336
Poseidis <vincent.c.wei@gmail.com>
2021-11-07 14:40:46 -0600

    consolidated basic csv_write test output

--- test/backend/csv-raw-test.js
+++ test/backend/csv-raw-test.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
-const sensor_names = require('sensor_names.json');
+const sensornames = require('./sensor_names.json');
 
 let colHeads, currentRow;
 
 let delim = ['\n', ',']; // Row and column delimiters, respectively
 
+
+let path = "./test/backend/csv_write/csv-fs.csv"
 /**
  * Joins the elements of the given array into a single string using global delims.
  * Used to format a given array into CSV format.
@@ -16,9 +18,9 @@ function _csvRowFormat(csvArr) {
 }
 
 // Create new file with array of strings as columne titles
-colHeads = ['drive','asym','eq_low','eq_mid','eq_high','gain']; // 6
+colHeads = sensornames.sensor_names//['drive','asym','eq_low','eq_mid','eq_high','gain']; // 6
 currentRow = _csvRowFormat(colHeads);
-fs.writeFile('csv-fs.csv', currentRow, function (err) {
+fs.writeFile(path, currentRow, function (err) {
     if (err) throw err;
     console.log(
         `New file added:
@@ -32,13 +34,14 @@ fs.writeFile('csv-fs.csv', currentRow, function (err) {
  * @param {number} i
  */
 function _generateValues(i) {
-    return [i * 0.1, i * 0.1 * (-1)**i, 0.5^i, 1, 2*i, 10^i];
+    return [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37];
+    //return [i * 0.1, i * 0.1 * (-1)**i, 0.5^i, 1, 2*i, 10^i];
 }
 
 // Add value rows into CSV
 for (let i = 0; i < 4; i++) {
     currentRow = _csvRowFormat(_generateValues(i))
-    fs.appendFile('csv-fs.csv', currentRow, function (err) {
+    fs.appendFile(path, currentRow, function (err) {
         if (err) throw err;
         console.log(currentRow);
     });

7085e5cef509bce8f56f0d1c52731688570d5324
a11ce <a11ce@protonmail.com>
2021-11-15 19:44:57 -0600

    Working example client, remove broken stuff

---
+++ server/exampleClient.js
@@ -0,0 +1,19 @@
+const net = require('net')
+const readline = require('readline');
+
+const clientSock = new net.Socket();
+clientSock.connect({port: 42069, host: 'localhost'}, () =>
+{
+    console.log("connected");
+})
+
+clientSock.on('data', (data) =>
+{
+    console.log(data.toString('ascii'));
+})
+
+const rl = readline.createInterface({input: process.stdin})
+rl.on('line', (line) =>
+{
+    clientSock.write(line)
+})

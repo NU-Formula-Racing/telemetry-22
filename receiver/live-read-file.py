8f63df6794c9cfe05282610f22b900e9d07c1e2c
offi-DtrGuo-cial <offi.dtrguo.cial@gmail.com>
2021-11-22 20:07:21 -0600

    Live read test start
    Started test file for live reading of live-written file

---
+++ receiver/live-read-file.py
@@ -0,0 +1,47 @@
+# from threading import Thread
+import os
+import time
+import random as rand
+
+# os.chdir('./live-read-dir/')
+# os.path.dirname(os.path.abspath(__name__)) # __name__ on Mac, change to __file__ for Windows and Linux
+
+# Generate file path
+path = os.path.dirname(os.path.abspath(__name__))
+if 'receiver' not in path:
+    path = os.path.join(path, 'receiver')
+path = os.path.join(path, 'live-read-dir/lr-test.bin')
+
+# Create new file
+with open(path, 'w') as lr_file:
+    print('File created: lr-test.bin')
+
+# Seeding RNG to make results consistent
+rand.seed(1337)
+
+term = b'\x13\x37' # Line terminator
+count = 0 # Number of characters generated per line, used to determine terminator placement
+c_range = 16 # [0, c_range]; range of number of characters generated per cycle
+c_lim = 64 # Number of characters per "line," including terminator, used for reading and terminator placement
+
+# Append to file live with bits
+with open(path, 'ab') as lr_file:
+    while True:
+        if count >= c_lim - len(term): 
+            # count has currently exceeded c_lim as shown below, needs to be reset
+            count %= c_lim - len(term) # Rectify count; terminator already accounted for
+            bit_stream = rand.randbytes(count) # Use excess in count to generate value
+        else:
+            # Generate random number of new bits
+            c_add = rand.randint(0, 16)
+            if count + c_add < c_lim - len(term):
+                # Count currrently exceeds terminator amount
+                # Uses previous count value in generation
+                # Generates bytes up to c_lim - length of terminator, then adds terminator
+                bit_stream = rand.randbytes(c_lim - len(term) - count) + term
+            else:
+                bit_stream = rand.randbytes(c_add)
+            count += c_add # Actually add to count
+        print(bit_stream)
+        time.sleep(0.2)
+        lr_file.write(bit_stream)

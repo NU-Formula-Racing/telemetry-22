e2eaeec282bea81350bb0a64ec5bd2dd1c6910f5
offi-DtrGuo-cial <offi.dtrguo.cial@gmail.com>
2021-12-01 19:32:41 -0600

    Adaptation of live-read tests to JSON

--- receiver/live-read-file.py
+++ receiver/live-read-file.py
@@ -8,6 +8,46 @@ import random as rand
 # os.chdir('./live-read-dir/')
 # os.path.dirname(os.path.abspath(__name__)) # __name__ on Mac, change to __file__ for Windows and Linux
 
+'''JSON Version'''
+# Generate file path
+path = os.path.dirname(os.path.abspath(__name__))
+if 'receiver' not in path:
+    path = os.path.join(path, 'receiver')
+path = os.path.join(path, 'live-read-dir/lr-test.txt')
+
+# Create new file
+with open(path, 'w') as lr_file:
+    print('File created: lr-test.txt')
+
+# Seeding RNG to make results consistent
+rand.seed(1337)
+
+
+
+# Append to file live with bits
+while True:
+    if count >= c_lim - len(term): 
+        # count has currently exceeded c_lim as shown below, needs to be reset
+        count %= c_lim - len(term) # Rectify count; terminator already accounted for
+        bit_stream = rand.randbytes(count) # Use excess in count to generate value
+    else:
+        # Generate random number of new bits
+        c_add = rand.randint(0, 16)
+        if count + c_add > c_lim - len(term):
+            # Count currrently exceeds terminator amount
+            # Uses previous count value in generation
+            # Generates bytes up to c_lim - length of terminator, then adds terminator
+            bit_stream = rand.randbytes(c_lim - len(term) - count) + term
+        else:
+            bit_stream = rand.randbytes(c_add)
+        count += c_add # Actually add to count
+    print(list(bit_stream))
+    time.sleep(0.1)
+    with open(path, 'ab') as lr_file:
+        lr_file.write(bit_stream)
+
+'''Bit Version'''
+'''
 # Generate file path
 path = os.path.dirname(os.path.abspath(__name__))
 if 'receiver' not in path:
@@ -47,3 +87,4 @@ while True:
     time.sleep(0.1)
     with open(path, 'ab') as lr_file:
         lr_file.write(bit_stream)
+'''

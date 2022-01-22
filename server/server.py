59a7e168414df016f83f8e17a31ab9524554eded
Poseidis <vincent.c.wei@gmail.com>
2022-01-22 16:04:16 -0600

    Scuffed session start implementation

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ import threading
 import json
 
 from response_files.list_sensors_by_subteam import list_sensors_by_subteam
+import test_start
 
 client = []
 
@@ -10,6 +11,8 @@ class ThreadedServer(object):
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.data = {}
+        self.session = False
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
@@ -38,7 +41,7 @@ class ThreadedServer(object):
                 client.close()
                 return
             
-            pyObjResp = responseToMessage(message)
+            pyObjResp = self.responseToMessage(message)
             if pyObjResp:
                 sendJson(client, pyObjResp)
             else:
@@ -50,31 +53,51 @@ class ThreadedServer(object):
 # MOVE THIS SOMEWHERE ELSE
 # (BECAUSE IT DOES NOT FIT HERE)
 # IF YOU USE THIS CODE
-def responseToMessage(message):
-    match message.split():
-        case ["LIST_SENSORS_BY_SUBTEAM"]:
-            return list_sensors_by_subteam()
-        case ["VALS", *sensorIDs]:
-            ## TODO: Cerberus - receiver
-            return {sensorID : 420 for sensorID in sensorIDs}
-        case ["STATUS"]:
-            ## TODO
-            return ":)"
-        case ["LIST_HISTORIC_DATAFILES"]:
-            ## Awaiting Cloud API
-            return [["name1", 420], ["name2", 69]]
-        case ["REQUEST_HISTORIC_DATAFILE_BY_TIME", timestamp]:
-            ## Awaiting Cloud API
-            return "no lol"
-        case ["REQUEST_HISTORIC_DATAFILE_BY_NAME", name]:
-            ## Awaiting Cloud API
-            return "no lol"
-        case ["END_SESSION", name]:
-            return ":)"
-        case ["BEGIN_SESSION"]:
-            return ":)"
-        case _:
-            return None
+    def responseToMessage(self, message):
+        match message.split():
+            case ["LIST_SENSORS_BY_SUBTEAM"]:
+                return list_sensors_by_subteam()
+            case ["VALS", *sensorIDs]: 
+                # This whole experiment was based on the premise that threads share memory, but apprently
+                # changes done by the thread created in "test_start.py" are not seen by further messages
+                # Made responseToMessage a method of ThreadedServer, then created data and session variables
+                # Tried starting the session, having the thread read from a file and into data.
+                # Tried monitoring the session variable to signal the thread reading from a file to stop
+                # if the session was to end.
+                print(type(self.data))
+                print(self.data)
+                ## TODO: Cerberus - receiver ---- Cerberus - parser?
+                if self.session:
+                    return  #{str(sensorID) : self.data[str(sensorID)] for sensorID in list(sensorIDs)}
+                else:
+                    return "SESSION NOT STARTED"
+                
+            case ["STATUS"]:
+                ## TODO
+                return ":)"
+            case ["LIST_HISTORIC_DATAFILES"]:
+                ## Awaiting Cloud API
+                return [["name1", 420], ["name2", 69]]
+            case ["REQUEST_HISTORIC_DATAFILE_BY_TIME", timestamp]:
+                ## Awaiting Cloud API
+                return "no lol"
+            case ["REQUEST_HISTORIC_DATAFILE_BY_NAME", name]:
+                ## Awaiting Cloud API
+                return "no lol"
+            case ["END_SESSION", name]:
+                ## TODO
+                self.session = False
+                return ":)"
+            case ["BEGIN_SESSION"]:
+                ## TODO: Enables connection to receiver/parser, starts test_start
+                if not self.session:
+                    self.session = True
+                    test_start.start_session(self)
+                    return "SESSION STARTED"
+                else:
+                    return "SESSION ALREADY ACTIVE"
+            case _:
+                return None
             
 def getMessage(client):
     raw = client.recv(1024)

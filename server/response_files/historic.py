1ff0555ed2ccea2a80d5569de7767db90e5bab6e
offi-DtrGuo-cial <derekguo31@gmail.com>
2022-02-12 13:51:07 -0600

    Reorganization of some files
    Moved some clutter and finalized the merge

---
+++ server/response_files/historic.py
@@ -0,0 +1,36 @@
+import os
+
+def set_state(self, cloud):
+    self.cloud = cloud
+    # TODO: what other things need to be here?
+    return True
+
+def list_local_data_files(path):
+    if not os.path.isdir(path):
+        return False
+    files = os.listdir(path)
+    for dir in files[os.path.isdir(files[:])]: # Find all subdirecfories in the path
+        files.append(list_local_data_files(path + '/' + dir)) # Check all files in each subdirectory
+    return files[files[:].endswith('.csv')] # Filter by CSV files
+
+def find_local_files_by_name(path, name):
+    files = list_local_data_files(path)
+    if files:
+        files = files[name in files]
+        json_f = {}
+        for file in files:
+            json_f[file] = file#.timestamp?
+        return json_f
+    else:
+        return False
+
+def find_local_files_by_dt(path, dt):
+    files = list_local_data_files(path)
+    if files:
+        files = files[files == 0] #files[:].timestamp == dt? or within a certain range
+        json_f = {}
+        for file in files:
+            json_f[file] = file#.timestamp?
+        return json_f
+    else:
+        return False

5012ce67ae41d32cf512f5f8af0749321f7a3f94
Poseidis <vincent.c.wei@gmail.com>
2022-02-26 14:59:33 -0600

    historic data name search

--- server/exampleClient.js
+++ server/exampleClient.js
@@ -12,6 +12,11 @@ clientSock.on('data', (data) =>
     console.log(data.toString('ascii'));
 })
 
+clientSock.on('error', (error) =>
+{
+    console.log('ERROR: ' + error.toString('ascii'));
+})
+
 const rl = readline.createInterface({input: process.stdin})
 rl.on('line', (line) =>
 {
--- server/response_files/local_historic.py
+++ server/response_files/local_historic.py
@@ -16,10 +16,10 @@ def list_local_data_files(path):
 def find_local_files_by_name(path, name):
     files = list_local_data_files(path)
     if files:
-        files = files[name in files]
         json_f = {}
         for file in files:
-            json_f[file] = file#.timestamp?
+            if boyer_moore_match(file, name) >= 0:
+                json_f[file] = os.stat(file).st_ctime
         return json_f
     else:
         return False
@@ -27,10 +27,53 @@ def find_local_files_by_name(path, name):
 def find_local_files_by_dt(path, dt):
     files = list_local_data_files(path)
     if files:
-        files = files[files == 0] #files[:].timestamp == dt? or within a certain range
+        #files = files[files == 0] #files[:].timestamp == dt? or within a certain range
+        file = files[os.stat(file).st_ctime == dt]
         json_f = {}
         for file in files:
-            json_f[file] = file#.timestamp?
-        return json_f
+            dt = open(file).readline()
+            json_f[file] = os.stat(file).st_ctime
+        # sort dictionary by closest to timestamp
+        return sorted(json_f.items)
     else:
         return False
+
+def boyer_moore_match(text, pattern):
+    """Find occurrence of pattern in text."""
+    alphabet = set(text)
+    last = last_occurrence(pattern, alphabet)
+    m = len(pattern)
+    n = len(text)
+    i = m - 1  # text index
+    j = m - 1  # pattern index
+    while i < n:
+        if text[i] == pattern[j]:
+            if j == 0:
+                return i
+            else:
+                i -= 1
+                j -= 1
+        else:
+            l = last(text[i])
+            i = i + m - min(j, 1+l)
+            j = m - 1 
+    return -1
+
+class last_occurrence(object):
+    """Last occurrence functor."""
+
+    def __init__(self, pattern, alphabet):
+        """Generate a dictionary with the last occurrence of each alphabet
+        letter inside the pattern.
+        
+        Note: This function uses str.rfind, which already is a pattern
+        matching algorithm. There are more 'basic' ways to generate this
+        dictionary."""
+        self.occurrences = dict()
+        for letter in alphabet:
+            self.occurrences[letter] = pattern.rfind(letter)
+
+    def __call__(self, letter):
+        """Return last position of the specified letter inside the pattern.
+        Return -1 if letter not found in pattern."""
+        return self.occurrences[letter]
